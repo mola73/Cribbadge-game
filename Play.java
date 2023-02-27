f4c9494a2cea220b0c6d8afd6db1ba56763b780b
MateusPAbreu <122947765+MateusPAbreu@users.noreply.github.com>
2023-02-26 23:19:11 -0300

    Add files via upload

--- Play.java
+++ Play.java
@@ -4,9 +4,10 @@
  * and open the template in the editor.
  */
 
-
 //import static com.mycompany.score.Card.getCard;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -17,6 +18,7 @@ import java.util.Scanner;
 public class Play {
 
     private static int count = 0;
+    private static int playSum = 0;
 
     public static void play(Player human, Player cpu) {
 
@@ -66,8 +68,18 @@ public class Play {
                 cpick = rand.nextInt(x.getCards().size() + 1);// pick random number from 1 to4, then from 1 to 3... 
             }
 
+            if (go(x, cpick)) {//VERIFIES IF THE CARD BEING PUT DOWN DOES NOT SURPASS 31, AND IF IT DOES, IT CALLS GO
+                play.clear();
+                System.out.println(x + " Says: GO!");
+                continue;
+            }
+
             play.add(x.getCards().get(cpick - 1));//put the selected card into the arraylist called play to keep track of the placed cards
 
+            playSum = playSum + x.getCards().get(cpick - 1).getRank().count();
+            
+            //CHECK METHOD SHOULD BE AROUND HERE
+            
             if (x.ruHumanb()) {//delete the given card
                 hcards.remove(cpick - 1);
             }
@@ -77,32 +89,125 @@ public class Play {
             tnumb++;// move to the next turn
             System.out.println(play);
 
+            if (playSum >= 31) {
+                play.clear();
+                playSum = 0;
+            }
         }
     }
 
     public static int pair(ArrayList<Card> c1) {
-if(c1.size()>=2){
-        if (c1.get(count).getRank() == c1.get(count + 1).getRank()) {
-            System.out.println(c1.get(count).getRank() +"  " + c1.get(count + 1).getRank());
-            count++;
-            return 2;
+        if (c1.size() >= 2) {
+            if (c1.get(count).getRank() == c1.get(count + 1).getRank()) {
+                System.out.println(c1.get(count).getRank() + "  " + c1.get(count + 1).getRank());
+                count++;
+                return 2;
 
+            }
+            count++;
+            return 0;
         }
-        count++;
         return 0;
     }
-return 0;
+
+    public static int threePair(ArrayList<Card> c1) {
+        if (c1.size() >= 3) {
+            if (c1.get(count).getRank() == c1.get(count + 1).getRank() && c1.get(count).getRank() == c1.get(count + 2).getRank()) {
+                System.out.println(c1.get(count).getRank() + "  " + c1.get(count + 1).getRank());
+                count++;
+                return 6;
+
+            }
+            count++;
+            return 0;
+        }
+        return 0;
     }
 
+    public static int fourPair(ArrayList<Card> c1) {
+        if (c1.size() >= 4) {
+            if (c1.get(count).getRank() == c1.get(count + 1).getRank() && c1.get(count).getRank() == c1.get(count + 2).getRank() && c1.get(count).getRank() == c1.get(count + 3).getRank()) {
+                System.out.println(c1.get(count).getRank() + "  " + c1.get(count + 1).getRank());
+                count++;
+                return 12;
+
+            }
+            count++;
+            return 0;
+        }
+        return 0;
+    }
+    
+    public static boolean sequncheck(ArrayList<Card> c1){
+        
+        
+    }
+
+    public static int run(ArrayList<Card> c1) {
+        ArrayList<Integer> sortedCards = new ArrayList();
+        
+        for(Card c: c1){
+            sortedCards.add(c.getRank().count());
+            Collections.sort(sortedCards);
+            
+        }
+        if(sortedCards.size()>2) {
+            for(int i=0; i< sortedCards.size(); i++){
+                while(sortedCards.get(i)+1 == sortedCards.get(i+1)){
+                    
+                    i++; 
+                }
+                
+//                if(sortedCards.get(i)+1 == sortedCards.get(i+1)) {//&& sortedCards.get(i)+1 == sortedCards.get(i+2)-1){
+//                    return 3;
+//                }
+            }
+        }
+        return 0;
+    }
+
+    public static int fifteen(ArrayList<Card> c1) {
+        int sum = 0;
+        for (Card c : c1) {
+            sum = sum + c.getRank().count();
+            if (sum == 15) {
+                return 2;
+            }
+        }
+        return 0;
+    }
+
+    public static boolean go(Player p1, int cpick) {
+        if (playSum + p1.getCards().get(cpick - 1).getRank().count() > 31) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static int total31(ArrayList<Card> hCard, ArrayList<Card> cCard, int playSum){//the ArrayList used here is either hcards or cpucards
+        if(playSum == 31 && hCard.size() + cCard.size() == 1){
+            return 2;
+        }else{
+            return 0;
+        }
+    }
+    
+    public static int less31(ArrayList<Card> hCard, ArrayList<Card> cCard, int playSum){//the ArrayList used here is either hcards or cpucards
+        if(playSum<31 && hCard.size() + cCard.size() == 1){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+    
     public static void main(String[] args) {// IT WORKS
         ArrayList<Card> card1 = new ArrayList();
         Random rnd = new Random();
         for (int i = 0; i < 4; i++) {
             card1.add(Card.getCard(rnd.nextInt(52)));
-            System.out.println(card1);
-               System.out.println(pair(card1));
+            run(card1);
         }
-        
-     
+
     }
 }
